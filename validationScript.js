e03fa0f6ccc1b22c44d18ec286c8ac1f4604d12c
Q456lm <cbbunting1@my.waketech.edu>
2025-03-31 11:11:09 -0400

    Added Code

--- validationScript.js
+++ validationScript.js
@@ -10,3 +10,26 @@
         // Valid input: display confirmation and submit the form
 
         // Invalid input: display error message
+
+let inp = document.getElementById("inputField");
+let sub = document.getElementById("submit");
+let fom = document.getElementById("myForm");
+let hed = document.getElementById("head");
+let chc = false
+console.log(chc)
+console.log()
+
+sub.addEventListener("click",check)
+
+
+function check(){
+  format = /^[a-z0-9]+$/i;
+  if (!(format.test(inp.value))){
+    inp.setCustomValidity("Please, enter alphanumeric input");
+  } else{
+    inp.setCustomValidity('')
+    fom.style.display = "none";
+    hed.innerHTML = "You did it!"
+    event.preventDefault()
+  }
+}
